28b38a2a8ff509750c070fe9a4a2b85c6ca061cd
saetan <saetan@live.com>
2020-10-03 23:31:51 +0800

    edited models.DatetimeField and added Serializers

--- DiscordHooker/api_basic/models.py
+++ DiscordHooker/api_basic/models.py
@@ -6,4 +6,9 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     authour = models.CharField(max_length=50)
     email = models.EmailField(max_length=255)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
     
