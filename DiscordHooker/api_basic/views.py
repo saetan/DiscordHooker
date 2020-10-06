f6cd3605574938225711eeb839304ffac4e50223
saetan <saetan@live.com>
2020-10-06 16:31:55 +0800

    added urls and views

--- DiscordHooker/api_basic/views.py
+++ DiscordHooker/api_basic/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home_page(request):
+
+    return HttpResponse("<h1>Hello World</h1><script>x = 10; x += 10; console.log(x);</script>")
